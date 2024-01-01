7e00fb4e77f1d84b26164e8e763c1032ee71f0f4
Mila Lüscher <mila.luescher@gmail.com>
2024-01-01 21:14:10 +0100

    Upload again after plugin fix

---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuroEnemy : public AAuroCharacterBase, public IEnemyInterface
+{
+	GENERATED_BODY()
+
+public:
+	AAuroEnemy();
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+
+
+
+	
+};
