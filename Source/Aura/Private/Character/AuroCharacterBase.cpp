8e0d918c8d5f235d5c79d1652a4406f35310c024
Mila Lüscher <mila.luescher@gmail.com>
2024-01-01 14:02:42 +0100

    added character classes

---
+++ Source/Aura/Private/Character/AuroCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuroCharacterBase.h"
+AAuroCharacterBase::AAuroCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuroCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
